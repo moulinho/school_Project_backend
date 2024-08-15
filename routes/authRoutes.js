6cebee1a071bd51ce0bfb76bc119d0429a75b23d
moulinho <moulinho132@gmail.com>
2024-08-15 00:50:15 +0000

    release authModule

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 // Register a new user
 router.post("/register", async (req, res) => {
-  console.log("eq.body====>", req.body);
+  // console.log("eq.body====>", req.body);
 
   const { id, firstName, lastName, email, contact, address, city, password } =
     req.body;
@@ -46,7 +46,7 @@ router.post("/register", async (req, res) => {
         if (err) {
           res.status(500).send(err);
         } else {
-          console.log("results", results);
+          // console.log("results", results);
 
           // Generate JWT token
           const token = jwt.sign(
@@ -92,7 +92,7 @@ router.post("/login", async (req, res) => {
     // Check if user exists
     const query = "SELECT * FROM Customers WHERE email = ?";
 
-    db.query(query, [email], (err, results) => {
+    db.query(query, [email], async (err, results) => {
       if (err) {
         res.status(500).json({ error: err.message });
         return;
@@ -109,18 +109,21 @@ router.post("/login", async (req, res) => {
           expiresIn: process.env.JWT_EXPIRES_IN,
         }
       );
+      const validPassword = await bcrypt.compare(password, results[0].password);
+      if (!validPassword) {
+        return res.status(400).json({ message: "Invalid credentials" });
+      }
 
       res.status(200).json({
         token: token,
-        results: { ...results },
+        results: results.filter((item) => {
+          delete item.id;
+          delete item.password;
+          return item;
+        }),
       });
     });
     // console.log("user===>", user);
-
-    const validPassword = await bcrypt.compare(password, user[0].password);
-    if (!validPassword) {
-      return res.status(400).json({ message: "Invalid credentials" });
-    }
   } catch (err) {
     console.error("SQL Error:", err);
     res.status(500).json({ message: "Database error", error: err });
