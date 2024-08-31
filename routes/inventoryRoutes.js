f84d18c9a04375c0278173a7d7c1c8acadd8b3d7
moulinho <moulinho132@gmail.com>
2024-08-31 21:05:54 +0000

    api product in stock

---
+++ routes/inventoryRoutes.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const router = express.Router();
+const db = require("../database");
+const id = uuidv4();
+
+// Helper function to execute SQL queries
+const query = (sql, params) => {
+  return new Promise((resolve, reject) => {
+    db.query(sql, params, (error, results) => {
+      if (error) {
+        return reject(error);
+      }
+      resolve(results);
+    });
+  });
+};
+
+// Get all orders by User conect
+router.post("/decrease", async (req, res) => {
+  const items = req.body;
+
+  if (items && items.length > 0) {
+    try {
+      // Execute all queries
+      for (const item of items) {
+        const queryInventory = `
+        UPDATE Inventory 
+        SET quantity = quantity - ?
+        WHERE product_id = ?;
+      `;
+        await query(queryInventory, [item.quantity, item.id]);
+
+        const queryProduct = `
+        UPDATE Products
+        SET stock_quantity = stock_quantity - ?
+        WHERE id = ?;
+        `;
+        await query(queryProduct, [item.quantity, item.id]);
+      }
+
+      res
+        .status(200)
+        .json({ message: "Stock quantities updated successfully" });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  } else {
+    res.status(400).json({ message: "No items provided" });
+  }
+});
+
+// Get all orders by User conect
+router.get("/:id", async (req, res) => {
+  try {
+    const query = "";
+    res.json(orders);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Get all orders by User conect
+router.get("/:id", async (req, res) => {
+  try {
+    const query = "";
+    res.json(orders);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 //   .connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
 //   .then(() => console.log("MongoDB connected"))
 //   .catch((err) => console.error(err));
-  
+
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -47,6 +47,7 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/orders", require("./routes/orderRoutes"));
 app.use("/api/auths", require("./routes/authRoutes"));
 app.use("/api/paiment", require("./routes/paiementRoute"));
+app.use("/api/inventory", require("./routes/inventoryRoutes"));
 app.use("/api/shipping", require("./routes/shippingRoute"));
 
 app.listen(PORT, () => {
