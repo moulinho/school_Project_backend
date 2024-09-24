8b40abcc6f26fdcc9f509e3d4d1ee731cd3a619a
moulinho <moulinho132@gmail.com>
2024-09-24 22:38:09 +0000

    check state connection

--- database.js
+++ database.js
@@ -1,23 +1,36 @@
 const mysql = require("mysql2");
 const dotenv = require("dotenv");
 dotenv.config();
-const db = mysql.createPool({
+
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
   port: 3306,
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error("Error connecting to MySQL:", err);
-    return;
+// Connect to the database
+function connect() {
+  db.connect((err) => {
+    if (err) {
+      console.error("Error connecting to MySQL:", err);
+      setTimeout(connect, 2000); // Retry connection after 2 seconds
+    } else {
+      console.log("Connected to MySQL");
+    }
+  });
+}
+
+// Keep the connection alive
+db.on('error', (err) => {
+  console.error("Database error:", err);
+  if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+    connect(); // Reconnect on lost connection
   }
-  // console.log("Connected to MySQL");
 });
+
+// Initial connection
+connect();
 
 module.exports = db;
